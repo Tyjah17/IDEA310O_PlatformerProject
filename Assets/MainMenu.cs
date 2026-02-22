bc55b85ab4be2419bfe3f603f11be47b70d1f826
Tyjah Duncan <tyjah.duncan17@gmail.com>
2026-02-21 22:36:15 -0700

    MainMenu Functionality

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayLevel1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void PlayLevel2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void PlayLevel3()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
