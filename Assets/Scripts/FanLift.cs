3afb43dbe2645fca2a9adaec99a5acc25e572cf3
Tyjah Duncan <tyjah.duncan17@gmail.com>
2026-02-27 18:07:45 -0700

    Level1 and Level2 complete

---
+++ Assets/Scripts/FanLift.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:120fdebc7de1d729f219e4123ab139406d7569358303c62f47cb24fe2a264674
+size 542
---
+++ Assets/Scripts/FanReceiver.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:04972fb6acb846a95ac8cb6e13d0f1ba04001f135451e7578f52146ecfa68d3b
+size 282
