31d93c5224685b20d29f14192b641c1c8c3d67b8
Tyjah Duncan <tyjah.duncan17@gmail.com>
2026-03-07 22:09:28 -0700

    Implemented battery collection noise

--- Assets/Scripts/BatteryCollector.cs
+++ Assets/Scripts/BatteryCollector.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 public class BatteryCollector : MonoBehaviour {
     public TMP_Text BatteryText;
     public int BattsPerLvl = 5;
+    public AudioClip pickupSound;
     private int batteriesCollected = 0;
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Battery")) {
+            AudioSource.PlayClipAtPoint(pickupSound, other.transform.position);
             batteriesCollected++;
             BatteryText.text = "BATTERIES COLLECTED: " + batteriesCollected + "/" + BattsPerLvl;
             Destroy(other.gameObject);
